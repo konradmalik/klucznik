b1d131bc29d6072ec9444869fa5c90a0785cc393
Konrad Malik <konrad.malik@gmail.com>
2022-11-23 21:30:54 +0100

    feat: add url and ssh dir functions + test them

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::{path::PathBuf, time::Duration};
+
+type Result<T> = anyhow::Result<T>;
+
+const TIMEOUT: Duration = Duration::from_secs(5);
+
+fn main() -> Result<()> {
+    let ssh_dir = ssh_user_dir_location()?;
+    println!("ssh dir is: {}\n", ssh_dir.display());
+
+    let urls = vec![
+        "https://github.com/konradmalik.keys",
+        "https://gitlab.com/konradmalik.keys",
+    ];
+
+    for url in urls {
+        println!("getting keys from {}\n", url);
+
+        let keys = url_contents_to_string(url, TIMEOUT)?;
+
+        println!("{}\n", keys);
+    }
+    Ok(())
 }
 
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub fn url_contents_to_string(url: &str, timeout: Duration) -> Result<String> {
+    let req = ureq::get(url).timeout(timeout).call()?;
+    req.into_string().map_err(anyhow::Error::from)
+}
+
+pub fn ssh_user_dir_location() -> Result<PathBuf> {
+    directories::UserDirs::new()
+        .ok_or(anyhow::format_err!("cannot get HOME dir for current user"))
+        .map(|ud| ud.home_dir().join(".ssh"))
 }
 
 #[cfg(test)]
@@ -12,7 +40,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
+    fn test_user_ssh_dir() {
+        let ssh_dir = ssh_user_dir_location();
+        assert!(ssh_dir.is_ok(), "ssh dir must be returned");
+        assert!(
+            ssh_dir.unwrap().is_dir(),
+            "ssh dir must exist and be a directory"
+        );
     }
 }
